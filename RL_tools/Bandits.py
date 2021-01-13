d685e8176d4e68c341fa5e8490aaf4fc2e2bdb6d
s2091069 <s2091069@ed.ac.uk>
2021-01-13 19:47:11 +0000

    gradient bandit

--- RL_tools/Bandits.py
+++ RL_tools/Bandits.py
@@ -3,45 +3,16 @@ import numpy as np
 
 class Bandit:
 
-    def __init__(self, n_actions, n_steps, reward_estimates_init, selection, step_size, epsilon, c):
+    def __init__(self, n_actions, n_steps, reward_estimates_init, step_size):
         self.n_actions = n_actions
         self.n_steps = n_steps
         self.step_size = step_size
-        self.selection = selection
-        self.epsilon = epsilon
-        self.c = c
 
-        self.n_action_selected = np.ones(n_actions)/1e5
+        self.n_action_selected = np.ones(n_actions) / 1e5
         self.reward_estimates = reward_estimates_init
+        self.preferences = np.zeros(n_actions)
         self.action = 0
         self.step = 1
-
-    def epsilon_greedy(self):
-        rnd = np.random.uniform()
-        if self.epsilon > rnd:
-            action = np.random.choice(np.arange(self.n_actions))
-        else:
-            action = self.reward_estimates.argmax()
-
-        self.n_action_selected[action] += 1
-        self.action = action
-        return action
-
-    def upper_confidence_bound(self):
-
-        action = np.argmax(self.reward_estimates + self.c * np.sqrt(np.log(self.step) / self.n_action_selected))
-
-        self.n_action_selected[action] += 1
-        self.action = action
-        return action
-
-    def select_action(self):
-        if self.selection == 'epsilon_greedy':
-            return self.epsilon_greedy()
-        elif self.selection == 'upper_confidence_bound':
-            return self.upper_confidence_bound()
-        else:
-            raise AttributeError("Unknown selection function")
 
     def update_reward_estimate(self, new_reward):
         current_reward = self.reward_estimates[self.action]
@@ -56,42 +27,80 @@ class Bandit:
         self.step += 1
 
 
+class EpsilonGreedyBandit(Bandit):
+
+    def __init__(self, n_actions, n_steps, reward_estimates_init, step_size, epsilon=0):
+        super().__init__(n_actions, n_steps, reward_estimates_init, step_size)
+        self.epsilon = epsilon
+
+    def select_action(self):
+        rnd = np.random.uniform()
+        if self.epsilon > rnd:
+            action = np.random.choice(np.arange(self.n_actions))
+        else:
+            action = self.reward_estimates.argmax()
+
+        self.n_action_selected[action] += 1
+        self.action = action
+        return action
+
+
+class UCBBandit(Bandit):
+
+    def __init__(self, n_actions, n_steps, reward_estimates_init, step_size, c=2):
+        super().__init__(n_actions, n_steps, reward_estimates_init, step_size)
+        self.c = c
+
+    def select_action(self):
+        action = np.argmax(self.reward_estimates + self.c * np.sqrt(np.log(self.step) / self.n_action_selected))
+
+        self.n_action_selected[action] += 1
+        self.action = action
+        return action
+
+
+class GradientBandit(Bandit):
+    def __init__(self, n_actions, n_steps, reward_estimates_init, step_size, baseline):
+        super().__init__(n_actions, n_steps, reward_estimates_init, step_size)
+        self.preferences = np.ones(n_actions) / n_actions
+        self.baseline = baseline
+
+    def select_action(self):
+        action = np.random.choice(np.arange(self.n_actions), p=self.probabilities())
+        self.n_action_selected[action] += 1
+        self.action = action
+        return action
+
+    def probabilities(self):
+        return np.exp(self.preferences) / np.sum(np.exp(self.preferences))
+
+    def update_preferences(self, reward):
+        alpha = 1 / self.n_action_selected[self.action] if self.step_size == 'mean' else self.step_size
+        self.preferences = self.preferences - alpha * (reward - self.reward_estimates) * self.probabilities()
+        self.preferences[self.action] = self.preferences[self.action] \
+                                        + alpha * (reward - self.reward_estimates[self.action])\
+                                        * (1-self.probabilities()[self.action])
+
+
 class Task:
 
-    def __init__(self, n_actions, n_steps, **kwargs):
+    def __init__(self, bandit_type, n_actions, n_steps, rewards, reward_estimates_init, step_size, **kwargs):
+        self.bandit_type = bandit_type
         self.n_actions = n_actions
         self.n_steps = n_steps
-        self.rewards = np.random.normal(size=n_actions)
-
-        if 'reward_estimates_init' in kwargs.keys():
-            reward_estimates_init = kwargs['reward_estimates_init']
-        else:
-            reward_estimates_init = np.zeros(n_actions)
-
-        if 'selection' in kwargs.keys():
-            selection = kwargs['selection']
-        else:
-            selection = 'epsilon_greedy'
-
-        if 'step_size' in kwargs.keys():
-            step_size = kwargs['step_size']
-        else:
-            step_size = 'mean'
-
-        if 'epsilon' in kwargs.keys():
-            epsilon = kwargs['epsilon']
-        else:
-            epsilon = 0.
-
-        if 'c' in kwargs.keys():
-            c = kwargs['c']
-        else:
-            c = 2
+        self.rewards = rewards
 
         self.optimal_action = self.rewards.argmax()
         self.step = 1
 
-        self.bandit = Bandit(n_actions, n_steps, reward_estimates_init, selection, step_size, epsilon, c)
+        if bandit_type == 'epsilon_greedy':
+            self.bandit = EpsilonGreedyBandit(n_actions, n_steps, reward_estimates_init, step_size, **kwargs)
+        elif bandit_type == 'upper_confidence_bound':
+            self.bandit = UCBBandit(n_actions, n_steps, reward_estimates_init, step_size, **kwargs)
+        elif bandit_type == 'gradient':
+            self.bandit = GradientBandit(n_actions, n_steps, reward_estimates_init, step_size, **kwargs)
+        else:
+            raise AttributeError("Unknown bandit type")
 
         self.reward_tracker = np.zeros(n_steps)
         self.optimal_action_tracker = np.zeros(n_steps)
@@ -99,11 +108,14 @@ class Task:
     def make_step(self):
         action = self.bandit.select_action()
         reward = self.rewards[action] + np.random.normal()
-        self.reward_tracker[self.step-1] = reward
-
-        if action == self.optimal_action:
-            self.optimal_action_tracker[self.step-1] += 1
         self.bandit.update_reward_estimate(reward)
+        if self.bandit_type == 'gradient':
+            self.bandit.update_preferences(reward)
+
+        # trackers
+        self.reward_tracker[self.step - 1] = reward
+        if action == self.optimal_action:
+            self.optimal_action_tracker[self.step - 1] += 1
 
     def run_task(self):
         for _ in range(self.n_steps):
--- RL_tools/main.py
+++ RL_tools/main.py
@@ -6,27 +6,35 @@ from Bandits import Task
 
 n_actions = 10
 n_steps = 1000
-n_runs = 1000
+n_runs = 100
 
 task_rewards = np.zeros((n_steps, n_runs))
 optimal_actions = np.zeros((n_steps, n_runs))
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    plt.rc('figure', figsize=(12,5))
+    plt.rc('figure', figsize=(12, 5))
 
-    for r in range(n_runs):
-        task = Task(n_actions, n_steps, selection='upper_confidence_bound')
+    for i in range(n_runs):
+        task = Task('gradient', n_actions, n_steps,
+                    rewards=np.random.normal(4, size=n_actions),
+                    reward_estimates_init=np.zeros(n_actions),
+                    step_size=0.1,
+                    baseline='mean')
         task.run_task()
-        task_rewards[:, r] = task.reward_tracker
+        optimal_actions[:, i] = task.optimal_action_tracker
 
-    plt.plot(task_rewards.mean(axis=1))
+    plt.plot(optimal_actions.mean(axis=1))
 
-    for r in range(n_runs):
-        task = Task(n_actions, n_steps, selection='epsilon_greedy', epsilon=0.1)
+    for i in range(n_runs):
+        task = Task('gradient', n_actions, n_steps,
+                    rewards=np.random.normal(4, size=n_actions),
+                    reward_estimates_init=np.zeros(n_actions),
+                    step_size=0.4,
+                    baseline='mean'
+                    )
         task.run_task()
-        task_rewards[:, r] = task.reward_tracker
+        optimal_actions[:, i] = task.optimal_action_tracker
 
-    plt.plot(task_rewards.mean(axis=1))
+    plt.plot(optimal_actions.mean(axis=1))
+
     plt.show()
